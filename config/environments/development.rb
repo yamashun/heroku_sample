d17f5a44a8d75ea7691d49da232cb758f3b47ce2
山田 俊輔 <yshunske@gmail.com>
2019-10-20 17:23:41 +0900

    sendgridの設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['GMAIL_ADDRESS'],
+  #   :password => ENV['GMAIL_PASSWORD'],
+  #   :address => 'smtp.gmail.com',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
