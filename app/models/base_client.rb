3a880987c1d079911fd0cac5f2b7e7c0a07f453f
山田 俊輔 <yshunske@gmail.com>
2019-10-21 10:22:04 +0900

    attr_encryptedのkeyを環境変数に変更

--- app/models/base_client.rb
+++ app/models/base_client.rb
@@ -2,6 +2,6 @@ class BaseClient < ApplicationRecord
   has_many :base_coupons
   belongs_to :user
 
-  attr_encrypted :client_secret, :code, :access_token, :refresh_token, key: Rails.application.credentials.base_client_key
+  attr_encrypted :client_secret, :code, :access_token, :refresh_token, key: Settings.base_client_key
   validates :client_id, presence: true
 end
