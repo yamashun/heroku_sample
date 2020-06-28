905bd102e3c2e662b2de412b6d9ca903956e7a00
yamashun <yshunske@gmail.com>
2020-06-28 19:45:20 +0900

    カバレッジのサマリをS3にアップロード

---
+++ app/services/coverage_s3_upload.rb
@@ -0,0 +1,21 @@
+class CoverageS3Upload
+  def upload
+    bucket = Aws::S3::Resource.new.bucket(ENV['COVERAGE_BUCKET'])
+    file = File.open("#{::Rails.root}/coverage/coverage_summary.json","r")
+    bucket.put_object(body: file, key: "all/#{Date.today.strftime('%Y-%m')}/coverage-#{Time.now.strftime('%d-%H-%M-%S')}.json")
+  end
+
+  private
+
+  def coverage_summary_json
+    file_path = "#{::Rails.root}/coverage/coverage.json"
+    hash = File.open(file_path) do |j|
+      JSON.load(j)
+    end
+
+    result = hash['metrics']
+    result['date'] = Date.today.strftime('%Y-%m-%d')
+    result['time'] = result['time'] = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    File.write("#{::Rails.root}/coverage/coverage_summary.json", result.to_json)
+  end
+end
